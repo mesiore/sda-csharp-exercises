acd293df01e3722bb0d2cfcfdee778fafb2f4861
mesiore <badzernowak@gmail.com>
2022-01-16 14:20:30 +0100

    dodałem zadanie

--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -7,14 +7,14 @@ namespace sda_csharp_exercises
         static void Main(string[] args)
         {
             {
-                Console.WriteLine("Podaj liczbę wyświetlonych linii:");
-                int _numberOfLines = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Podaj liczbę:");
+                int _number = Convert.ToInt32(Console.ReadLine());
 
                 int _price = 5;
 
-                for (int i = 0; i < _numberOfLines; i++)
+                for (int i = 0; i < _number; i++)
                 {
-                    Console.WriteLine($"Liczba sztuk: {i + 1}, łączna cena: {_price * (i + 1)}zł.");
+                    Console.WriteLine($"Liczba sztuk: {i + 1}, łączna cena: {_price * (i + 1)} zł.");
                 }
             }
         }
