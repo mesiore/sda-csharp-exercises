bcfae13695dc0d012b1e264b6ab3a26f08e64b08
mesiore <badzernowak@gmail.com>
2022-01-16 14:15:00 +0100

    zmienilem zadanie

--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -6,15 +6,16 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-            int quantity;
-            int price;
-
-            quantity = 3;
-            price = 5;
-
-            for (int i = 1; i <= quantity; i++)
             {
-                Console.WriteLine("Liczba sztuk: " + i + " łączna cena: " + i * price + "zł");
+                Console.WriteLine("Podaj liczbę wyświetlonych linii:");
+                int _numberOfLines = Convert.ToInt32(Console.ReadLine());
+
+                int _price = 5;
+
+                for (int i = 0; i < _numberOfLines; i++)
+                {
+                    Console.WriteLine($"Liczba sztuk: {i + 1}, łączna cena: {_price * (i + 1)}zł.");
+                }
             }
         }
     }
