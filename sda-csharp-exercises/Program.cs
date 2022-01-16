9d0073daee74e4e69f32557aa37dc22727960fce
mesiore <badzernowak@gmail.com>
2022-01-16 14:31:59 +0100

    Zadanie E02

--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -4,19 +4,34 @@ namespace sda_csharp_exercises
 {
     class Program
     {
+        static int PowerOf(int power, int number )
+        {
+            int value = number;
+            for (int i = 1; i < power; i++)
+            {
+                value = value * number;
+            }
+
+            return value;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Podstawa:");
+            int thisNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Potega:");
+            int thisPower = Convert.ToInt32(Console.ReadLine());
+
+            if (thisPower < 0)
             {
-                Console.WriteLine("Podaj liczbę:");
-                int _number = Convert.ToInt32(Console.ReadLine());
-
-
-                int _price = 5;
-
-                for (int i = 0; i < _number; i++)
-                {
-                    Console.WriteLine($"Liczba sztuk: {i + 1}, łączna cena: {_price * (i + 1)} zł.");
-                }
+                Console.WriteLine("Wykładnik nie może być ujenmy!");
+            }
+            else if (thisNumber == 0)
+            {
+                Console.WriteLine($"Wynik to: 1");
+            }
+            else
+            {
+                Console.WriteLine($"Wynik to: {PowerOf(thisNumber, thisPower)}");
             }
         }
     }
